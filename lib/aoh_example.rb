d1367327980f7aea78f6114e2df0bd56756193e0
ricky wahba <rjwahba@gmail.com>
2020-01-06 19:29:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,9 +26,13 @@ def literal_aoh
   :occupation=>"Live-in Staff"},
     {:name=>"Joelle van Dyne", 
   :occupation=>"Radio Personality"},
-    {:name=>"Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+    {:name=>"Pat Monteseian", 
+  :occupation=>"Staff" },
+    {:name=>"Kate Gompert",
+  :occupation=>"None"},
+    {:name=>"Bruce Green",
+  :occupation=>"Fan of Mildred"}
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
